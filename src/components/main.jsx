b373f80032268dbae5ab835d5624758d3f377598
Tigris95 <tigrisgithub95@gmail.com>
2019-10-16 00:00:11 +0200

    Start Project
    Create first files using create-react-app

---
+++ src/components/main.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Header from './header'
+// to do api
+
+class Main extends Component {
+    render() {
+        return (
+            <div>
+                <Header/>
+            Main Component Client Site
+            </div>
+        );
+    }
+}
+
+export default Main;
